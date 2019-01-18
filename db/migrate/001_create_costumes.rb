5ab0146afb5181b30f5f7be0894f5d178b91a1aa
tylerselldesign <tylerselldesign@gmail.com>
2019-01-18 22:49:02 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,4 +8,15 @@
 # In this change method, create columns with the correct names and 
 # value types according to the spec
 
-xkAA===
+class CreateCostumes < ActiveRecord::Migration[5.1]
+  
+  def update
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price 
+      t.integer :size 
+      t.string :image_url
+      t.
+  end
+  
+end
