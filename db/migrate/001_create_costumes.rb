19d12333bf6e6d4a0ba4577dd750d8411ad9c812
tylerselldesign <tylerselldesign@gmail.com>
2019-01-18 23:27:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,8 +14,8 @@ class CreateCostumes < ActiveRecord::Migration[5.0]
     create_table :costumes do |t|
       t.string :name
       t.float :price 
-     
-      t.string :image_url t.integer :size 
+      t.string :image_url
+      t.integer :size 
       t.timestamps 
     end
   end
