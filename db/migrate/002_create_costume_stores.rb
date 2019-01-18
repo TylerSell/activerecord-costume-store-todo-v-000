9a12d07882f112cde329561e3c777de081a8168e
tylerselldesign <tylerselldesign@gmail.com>
2019-01-18 22:54:09 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,9 @@
 # Create your costume_stores migration here
+
+class CostumeStores < ActiveRecord::Migration[5.1]
+  
+  def update
+    create_table :costume_stores do |t|
+      t.string :name 
+  
+end
