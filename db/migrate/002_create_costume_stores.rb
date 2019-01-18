f56f45939b0404f9ca1aef79ae7c2b05ab9db155
tylerselldesign <tylerselldesign@gmail.com>
2019-01-18 22:57:18 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,5 +5,13 @@ class CostumeStores < ActiveRecord::Migration[5.1]
   def update
     create_table :costume_stores do |t|
       t.string :name 
+      t.string :location 
+      t.integer :number_of_costumes 
+      t.integer :number_of_employees 
+      t.boolean :still_in_business 
+      t. :name 
+      t.string :name 
+    end
+  end
   
 end
